9dc7897bb7c83c4aa7c8638e4bdcda728a0a7101
merve <36377574+merveealpay@users.noreply.github.com>
2019-10-04 17:08:41 +0300

    add controller files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VeriTasiyicilar.Models;
+
+namespace VeriTasiyicilar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Kullanici model = new Kullanici { AdSoyad="Fatma Betül YILMAZ", Boy=178, DogumTarihi=new DateTime(1976, 12,12), Email="fbetulakdemirr@gmail.com", Id=1, Telefon="05437425342" };
+
+            ViewBag.Aciklama = "Kullanıcı bilgileri getirildi.";
+            ViewBag.SistemSaati = DateTime.Now;
+            ViewBag.AdSoyad = new Kullanici {AdSoyad= "Merve Alpay", Id=2 };
+
+            ViewData["Username"] = "fby";
+            ViewData["dict"] = new ViewDataDictionary { {"age","16"}, {"school","Fatih IT School" } };
+
+            return View(model);
+        }
+
+        public ActionResult Sepet()
+        {
+            TempData["Kitap"] = "Kitap sepete tempdata ile eklendi.";
+            ViewBag.Kitap = "Kitap sepete viewbag ile eklendi.";
+            ViewData["KitapData"] = "Kitap sepete viewdata ile eklendi.";
+            //return View();
+            return RedirectToAction("SatinAl");
+        }
+
+        public ActionResult SatinAl()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/KullaniciController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using VeriTasiyicilar.Models;
+using VeriTasiyicilar.Models.HazirListe;
+
+namespace VeriTasiyicilar.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        // GET: Kullanici
+        public ActionResult Index()
+        {
+            //polimorfism
+            List<Kullanici> listem= new KullaniciListesi();
+
+            return View(listem);
+        }
+
+        public ActionResult Detay(int id)
+        {
+            List<Kullanici> aranacakListe = new KullaniciListesi();
+            //where sorgusu listelenebilir formatta data döner
+            //aranacakListe.Where(x => x.Id == id).First();
+            //LINQ Sorgusu 
+            //Lambda Expression
+           Kullanici arananKisi = aranacakListe.Find(x => x.Id == id);
+            return View(arananKisi);
+        }
+    }
+}
