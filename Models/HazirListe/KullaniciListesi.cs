b9c881c0e7028f3bbc4568fd035f47f64af8edbb
merve <36377574+merveealpay@users.noreply.github.com>
2019-10-04 17:09:27 +0300

    add models

---
+++ Models/HazirListe/KullaniciListesi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VeriTasiyicilar.Models.HazirListe
+{
+    public class KullaniciListesi :List<Kullanici>
+    {
+        public KullaniciListesi()
+        {
+            this.Add(new Kullanici {Id=1, AdSoyad = "Büşra Zurnacı", DogumTarihi = new DateTime(1990, 12, 12) });
+            this.Add(new Kullanici {Id=2, AdSoyad = "Petek Okumuşoğlu", DogumTarihi = new DateTime(1990, 12, 12) });
+            this.Add(new Kullanici {Id=3, AdSoyad = "Yavuz Selim Çınar", DogumTarihi = new DateTime(1990, 12, 12) });
+            this.Add(new Kullanici {Id=4, AdSoyad = "Mehmet Turgut", DogumTarihi = new DateTime(1990, 12, 12) });
+        }
+    }
+}
---
+++ Models/Kullanici.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VeriTasiyicilar.Models
+{
+    public class Kullanici
+    {
+        public Kullanici()
+        {
+
+        }
+        public int Id { get; set; }
+        public string AdSoyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+        public int Boy { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+    }
+}
